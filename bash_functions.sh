83912440fde90aec67baba96eafdd84987e214e2
Naga <nagoclpl@gmail.com>
2019-05-27 11:08:57 -0700

    added more functions

--- bash_functions.sh
+++ bash_functions.sh
@@ -25,10 +25,27 @@ mo() { more "$@"; }
 tf() { tail -F "$@"; }
 fn() { find . -name "$@"; }
 
+rv() { ruby -v; }
 pv() { python -V; }
 gv() { go version; }
 jv() { java -version; }
+sv() { scala -version; }
 nv() { node -v; npm -v; }
 
 pip() { pip3 "$@"; }
 python() { python3 "$@"; }
+
+gl() { git log "$@"; }
+gd() { git diff "$@";}
+gs() { git status "$@"; }
+gc() { git commit "$@"; }
+
+gl1() { git log -1 "$@"; }
+gl2() { git log -2 "$@"; }
+gl3() { git log -3 "$@"; }
+gl4() { git log -4 "$@"; }
+gl5() { git log -5 "$@"; }
+gl6() { git log -6 "$@"; }
+gl7() { git log -7 "$@"; }
+gl8() { git log -8 "$@"; }
+gl9() { git log -9 "$@"; }
